b54078287e523415d3efe316b3a209226c631164
Tran Huu Khanh Van <kv090695@gmail.com>
2018-05-23 15:19:08 +0700

    update load more gifs

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,43 +13,55 @@ class App extends Component {
     constructor (props) {
         super(props)
         this.state = {
+            defaultCount: 20,
+            count: 20,
             gifs: []
         }
     }
 
     componentDidMount() {
-        axios.get(`http://api.giphy.com/v1/gifs/trending?&api_key=MsBDqHxbtScxCgSgupdEIKOLG9YHMCSb&limit=20`)
+        axios.get(`http://api.giphy.com/v1/gifs/trending?&api_key=MsBDqHxbtScxCgSgupdEIKOLG9YHMCSb`)
             .then(res => {
                 const data = res.data;
                 this.setState({
                     gifs: data.data
                 })
-                console.log(this.state.gifs[0].images.original.url)
             })
     }
 
-  render() {
-    return (
-      <div className="App">
-          <div className="heading">
-              <h1>Clone giphy app</h1>
-              <img className="icon" src={icon} alt=""/>
-          </div>
-          <div className="wrapper container-fluid">
-              <div className="row">
-                  { this.state.gifs.map(gif =>
-                      <BlockImage
-                          key={gif.id}
-                          imageLink={gif.images.original.url}
-                          username={gif.username}
-                          avatarLink={gif.user.avatar_url}
-                      />
-                  )}
-              </div>
-          </div>
-      </div>
-    );
-  }
+  //  Load more gifs
+
+    onLoadMore() {
+        let count = this.state.defaultCount;
+        count = count + this.state.count;
+        this.setState({ count });
+    }
+    render() {
+        const { gifs, itemCount} = this.state;
+        const count = this.state.count;
+        const showData = (item, index) => {
+            return ((index < count) ? <BlockImage
+                key={item.id}
+                imageLink={item.images.original.url}
+                username={item.username}
+                avatarLink={item.user.avatar_url}
+            /> : '');
+        };
+        return (
+            <div className="App">
+                <div className="heading">
+                    <h1>Clone giphy app</h1>
+                    <img className="icon" src={icon} alt=""/>
+                </div>
+                <div className="wrapper container-fluid">
+                    <div className="row">
+                        {gifs.map(showData)}
+                    </div>
+                    <div onClick={this.onLoadMore.bind(this)} className="loadMore">Load More</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
